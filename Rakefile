aed548ba10154b414db9e4f432510ffb60e8c4a5
Chris Scharf <scharfie@gmail.com>
2012-08-17 09:13:11 -0400

    jeweler

--- Rakefile
+++ Rakefile
@@ -1,52 +1,50 @@
+# encoding: utf-8
+
 require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
 require 'rake'
 
-begin
-  require 'jeweler'
-  Jeweler::Tasks.new do |gem|
-    gem.name = "yaml_store"
-    gem.summary = "A simple way to read your YAML files"
-    gem.description = gem.summary
-    gem.email = "scharfie@gmail.com"
-    gem.homepage = "http://github.com/scharfie/yaml_store"
-    gem.authors = ["Chris Scharf"]
-    gem.add_development_dependency "thoughtbot-shoulda"
-    # gem is a Gem::Specification... see http://www.rubygems.org/read/chapter/20 for additional settings
-  end
-rescue LoadError
-  puts "Jeweler (or a dependency) not available. Install it with: sudo gem install jeweler"
+require 'jeweler'
+Jeweler::Tasks.new do |gem|
+  # gem is a Gem::Specification... see http://docs.rubygems.org/read/chapter/20 for more options
+  gem.name = "yaml_store"
+  gem.homepage = "http://github.com/scharfie/yaml_store"
+  gem.license = "MIT"
+  gem.summary = %Q{TODO: one-line summary of your gem}
+  gem.description = %Q{TODO: longer description of your gem}
+  gem.email = "scharfie@gmail.com"
+  gem.authors = ["Chris Scharf"]
+  # dependencies defined in Gemfile
 end
+Jeweler::RubygemsDotOrgTasks.new
 
 require 'rake/testtask'
 Rake::TestTask.new(:test) do |test|
   test.libs << 'lib' << 'test'
-  test.pattern = 'test/**/*_test.rb'
+  test.pattern = 'test/**/test_*.rb'
   test.verbose = true
 end
 
-begin
-  require 'rcov/rcovtask'
-  Rcov::RcovTask.new do |test|
-    test.libs << 'test'
-    test.pattern = 'test/**/*_test.rb'
-    test.verbose = true
-  end
-rescue LoadError
-  task :rcov do
-    abort "RCov is not available. In order to run rcov, you must: sudo gem install spicycode-rcov"
-  end
+require 'rcov/rcovtask'
+Rcov::RcovTask.new do |test|
+  test.libs << 'test'
+  test.pattern = 'test/**/test_*.rb'
+  test.verbose = true
+  test.rcov_opts << '--exclude "gems/*"'
 end
 
-task :test => :check_dependencies
 task :default => :test
 
-require 'rake/rdoctask'
+require 'rdoc/task'
 Rake::RDocTask.new do |rdoc|
-  if File.exist?('VERSION')
-    version = File.read('VERSION')
-  else
-    version = ""
-  end
+  version = File.exist?('VERSION') ? File.read('VERSION') : ""
 
   rdoc.rdoc_dir = 'rdoc'
   rdoc.title = "yaml_store #{version}"
---
+++ test/test_yaml_store.rb
@@ -0,0 +1,133 @@
+require 'test_helper'
+
+class TestYamlStore < Test::Unit::TestCase
+  def assert_keys(expected_keys, results)
+    actual_keys = results.map { |e| e.key }
+    assert_equal expected_keys.sort, actual_keys.sort
+    # assert_equal expected_keys, actual_keys
+  end
+
+  context 'DSL' do
+    should "load test/data/cities.yml" do
+      YamlStore.load_paths = [
+        File.join(File.dirname(__FILE__), "data")
+      ]
+
+      @store = nil
+
+      assert_nothing_raised do
+        @store = YamlStore.from('cities')
+      end
+      
+      assert_equal 3, @store.records.length
+    end
+  end
+
+  context 'with hash' do
+    setup do
+      @store = YamlStore.new(YAML.load(SampleData.hash))
+    end
+
+    should "test having" do
+      assert_keys %w(A C), @store.having(:photo, true)
+      assert_keys %w(B D), @store.having(:photo, false)
+      assert_keys %w(A D), @store.having(:flag)
+      assert_keys %w(B),   @store.having(:age,   '<= 21')
+      assert_keys %w(C),   @store.having(:age,   '> 21')
+      assert_keys %w(A),   @store.having(:wage,  '= 5.65')
+      assert_keys %w(B C), @store.having(:first, /^Jo/)
+    end
+  
+    should "test not_having" do
+      assert_keys %w(B D), @store.not_having(:photo, true)
+      assert_keys %w(C),   @store.not_having(:last)
+      assert_keys %w(D),   @store.not_having(:age)
+      assert_keys %w(A D), @store.not_having(:first, /^Jo/)
+    end
+  
+    should "test having/not_having chain" do
+      assert_keys %w(A B C), @store.having(:first).not_having(:first, 'Chris')
+      assert_keys %w(A B),   @store.having(:first).not_having(:first, ['Chris', 'Joe'])
+      assert_keys %w(B D),   @store.having(:first).not_having(:first, /^\w{3}$/)
+    end
+  end
+  
+  context 'ordered map' do
+    setup do
+      @store = YamlStore.new(YAML.load(SampleData.ordered_map))
+    end
+  
+    should "test having" do
+      results = @store.having(:photo, true).fetch.map { |e| e.id }
+      assert results.include?('A')
+      assert results.include?('C')
+    end
+  end
+
+  context 'array' do
+    setup do
+      @store = YamlStore.new(YAML.load(SampleData.array))
+    end
+  
+    should "test having" do
+      results = @store.having(:photo, true).fetch.map { |e| e.id }
+      assert results.include?('A')
+      assert results.include?('C')
+    end
+  end
+end
+
+class SampleData
+  def self.ordered_map
+<<-YAML
+--- !omap
+- D:
+    photo: false
+- A:
+    photo: true
+- B: 
+    photo: false
+- C: 
+    photo: true
+YAML
+  end
+
+  def self.array
+<<-YAML
+- key: D
+  photo: false
+- key: A
+  photo: true
+- key: B
+  photo: false
+- key: C 
+  photo: true
+YAML
+  end  
+
+  def self.hash
+<<-YAML
+D:
+  photo: false
+  first: Chris
+  last:  Scharf
+  flag:  1
+A:
+  photo: true
+  first: Bob
+  last:  Smith
+  flag:  1
+  age:   nil
+  wage:  5.65
+B: 
+  photo: false
+  first: John
+  last:  Jones
+  age:   14
+C: 
+  photo: true
+  first: Joe
+  age:   95
+YAML
+  end
+end
